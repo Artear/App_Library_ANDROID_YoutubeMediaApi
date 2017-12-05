ab029cac836f5cae36a4275ff8327a78cebb2af5
sergiobanares <sergio.banares@gmail.com>
2017-12-05 15:52:44 -0300

    Change: Youtube Test 001

---
+++ youtubemediaapi/src/main/java/com/artear/youtubemediaapi/YouTubeMedia.java
@@ -0,0 +1,13 @@
+package com.artear.youtubemediaapi;
+
+import android.util.Log;
+
+/**
+ * Created by sergiobanares on 5/12/17.
+ */
+
+public class YouTubeMedia {
+    public YouTubeMedia() {
+        Log.d("YouTubeMedia", "Hello World");
+    }
+}
