093e19392290a1a677d02b4636f28b2dd5038d09
daver1419 <daver1419@gmail.com>
2017-12-11 18:14:58 -0300

    Add: Api and DeveloperKey. Change: YoutubeIntentService to call the api youtube

--- app/src/main/java/com/artear/app_library_android_youtubemediaapi/YoutubeIntentService.java
+++ app/src/main/java/com/artear/app_library_android_youtubemediaapi/YoutubeIntentService.java
@@ -2,7 +2,14 @@ package com.artear.app_library_android_youtubemediaapi;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class YoutubeIntentService extends IntentService {
@@ -18,6 +25,62 @@ public class YoutubeIntentService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
 
+        Intent intentResult = new Intent();
 
+        try{
+
+            String response = callYoutubeVideos();
+
+            if(response.isEmpty()){
+                intentResult.setAction(ACTION_ERROR);
+                sendBroadcast(intentResult);
+                return;
+            }
+
+            Log.d("YoutubeIntentService", response);
+            intentResult.setAction(ACTION_SUCCESS);
+
+        }catch (Exception e){
+            intentResult.setAction(ACTION_ERROR);
+        }
+
+        sendBroadcast(intentResult);
+    }
+
+    private String callYoutubeVideos() throws Exception {
+
+        StringBuilder response = new StringBuilder();
+
+        HttpURLConnection urlConnection;
+
+        Uri.Builder uriBuilder = Uri.parse(Api.BASE_URL).buildUpon();
+        uriBuilder.appendQueryParameter("part", "snippet");
+        uriBuilder.appendQueryParameter("chart" ,"mostPopular" );
+        uriBuilder.appendQueryParameter("regionCode", "AR");
+        uriBuilder.appendQueryParameter("maxResults", "25");
+        uriBuilder.appendQueryParameter("key", DeveloperKey.GOOGLE_KEY);
+
+        Uri uri = uriBuilder.build();
+
+        URL url = new URL(uri.toString());
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setReadTimeout(10000);
+        urlConnection.setConnectTimeout(15000);
+        urlConnection.setRequestMethod("GET");
+
+        urlConnection.connect();
+
+        if (urlConnection.getResponseCode() == 200) {
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                    urlConnection.getInputStream()));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                response.append(line);
+            }
+        }
+
+        return response.toString();
     }
 }
